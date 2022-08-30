45177c54887cb121e7eb8aa6dc7d88d9dd4b5498
Alvin Rumbaoa <alvinrumbaoa@gmail.com>
2022-08-29 22:16:41 -0700

    set

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import {Box, Heading, Text, Flex, Grid, Image, Link, Input, HStack, VStack, useColorMode, useColorModeValue} from'@chakra-ui/react'
+import {Box, Heading, Text, Flex, Grid, Image, Button,Link, Input, HStack, VStack, useColorMode, useColorModeValue} from'@chakra-ui/react'
 import styles from '../styles/Home.module.css'
 import React,{useState} from 'react';
 import {
@@ -12,26 +12,36 @@ import {
 export default function Home() {
 
   const [name, setName] = useState("MARY TESS");
-  const [cardNumber, setCardNumber] = useState("0");
+  const [cardNumber, setCardNumber] = useState("0000 0000 0000 0000 ");
   const [cardMonth, setCardMonth] = useState("0");
   const [cardYear, setCardYear] = useState("0");
   const [cvv, setCvv] = useState("000");
-
-
-  const isError = name === ''
+  const [cardNumError, setCardNumError] = useState(false)
   const isZero = cardNumber === '0' || ''
+  const isError = name === ''
+
+
+  const handleCardDisplay = () => {
+    const rawText = [...cardNumber.split(' ').join('')] // Remove old space
+    const creditCard = [] // Create card as array
+    rawText.forEach((t, i) => {
+        if (i % 4 === 0) creditCard.push(' ') // Add space
+        creditCard.push(t)
+    })
+    return creditCard.join('') // Transform card array to string
+}
 
   return (
-    <Flex h="100vh" direction="row"> 
+    <Flex h="100vh" direction={{sm:"column",lg:"row"}}> 
       <Head>
         <title>Interactive card details form</title>
         <meta name="description" content="Alvin Rumbaoa | Frontend Challenge" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Flex  w='600px' h="100%" bgColor="#21092F">
+      <Flex  w={{sm:"100%",lg:'700px'}} h={{sm:"30%",lg:"100%"}} bgColor="#21092F">
         <Image src="/images/Group 10.png"  w="683px"></Image>
       </Flex>
-      <Box w='500px' h='300px' boxShadow='xl' p='6' rounded='md'  ml={250} mt={100} pos="absolute" backgroundImage="url('/images/Oval Copy 2.png')" backgroundRepeat="no-repeat">
+      <Box w='500px' h='300px' boxShadow='xl' p='6' rounded='md'  ml={{sm:10,lg:250}} mt={{sm:-100,lg:100}} pos={{sm:"relative" ,lg:"absolute" }} backgroundImage="url('/images/Oval Copy 2.png')" backgroundRepeat="no-repeat">
         <Flex direction="row"> 
          <Box m="20px"bgColor="#DEDDDF" w="40px" h="40px" borderRadius="50%"/>
          <Box mt="27px" border=" 2px solid #DEDDDF" w="20px" h="20px" borderRadius="50%"/>
@@ -46,7 +56,7 @@ export default function Home() {
         
         
       </Box>
-      <Box w='500px' h='300px' ml={350} mt={500} pos="absolute" borderRadius="10px" bgColor="#D2D3D9">
+      <Box w='500px' h='300px'boxShadow='xl' ml={{sm:200, lg:350}} mb={{sm:100, lg:500}} pos={{sm:"inherit", lg:"absolute"}} borderRadius="10px" bgColor="#D2D3D9">
         <Box mt={10} bgColor="#2F2F2F" w="100%" h="50px" />
 
         <Box h="45px" bgColor="#ADB5BE" rounded="sm"  m={10}   >
@@ -60,6 +70,7 @@ export default function Home() {
         {/* name */}
       <FormLabel>CardHolder's Name</FormLabel>
         <Input
+          w="22em"
           type='text'
           onChange={(e) => setName(e.target.value)}
           placeholder='e.g. Mary Tess' 
@@ -74,9 +85,11 @@ export default function Home() {
         {/* cardnumber */}
          <FormLabel>CardHolder's Number</FormLabel>
         <Input
+          maxLength={20}
           type='text'
           onChange={(e) => setCardNumber(e.target.value)}
           placeholder='e.g. 1234 5678 9123 0000' 
+          value={handleCardDisplay()}
         />
 
         {!isError ? (
@@ -90,17 +103,20 @@ export default function Home() {
          <Box d="flex" justifuy="space-between" direction='row'>
          <Input m={2} w={20}
           type='text'
+          maxLength={2}
           onChange={(e) => setCardMonth(e.target.value)}
           placeholder='MM' 
         />
          <Input m={2} w={20}
           type='text'
+          maxLength={2}
           onChange={(e) => setCardYear(e.target.value)}
           placeholder='YY' 
         />
     
          <Input m={2} w={100}
           type='text'
+          maxLength={3}
           onChange={(e) => setCvv(e.target.value)}
           placeholder='e.g. 123' 
         />
@@ -115,7 +131,10 @@ export default function Home() {
         )}
         
        </FormControl>
+       <Button mt={5} p={5}bgColor="#21092F" color="white" ><Link >Confirm</Link></Button>
       </Flex>
+
+     
     </Flex>
   )
 }
